0b2bf4f87eaa09113ddf0632e2377dd4a8038841
Paul Downey <psd@whatfettle.com>
2017-02-24 09:11:42 +0000

    More GOV.UK fields

--- lists/govuk/map.py
+++ lists/govuk/map.py
@@ -3,13 +3,43 @@
 import json
 import sys
 
-fields = ['government-organisation', 'name', 'website']
+fields = ['government-organisation', 'abbreviation', 'government-organisation-type', 'govuk', 'name', 'website', 'parent-govuks']
 
-field_map = {
-    'name': 'title',
+field_path = {
     'government-organisation': 'analytics_identifier',
-    'website': 'web_url'
+    'government-organisation-type': 'format',
+    'govuk': 'details/slug',
+    'name': 'title',
+    'website': 'web_url',
+    'parent-govuks': 'parent-govuks',
+    'abbreviation': 'details/abbreviation'
 }
+
+def array_to_string(arr, pre_process):
+    res = ""
+    first = True
+    for i in arr:
+        if first:
+            first = False
+        else:
+            res += ";"
+        res += pre_process(i)
+    return res
+
+
+def json_path(d, path):
+    e = d
+    try:
+        for p in path.split("/"):
+            e = e.get(p)
+    except:
+        pass
+
+    if not e:
+        e = ''
+
+    return e
+
 
 sep = '\t'
 
@@ -18,5 +48,6 @@ print(sep.join(fields))
 for line in sys.stdin:
     r = json.loads(line)
 
-    row = {}
-    print(sep.join([r[field_map[field]] for field in fields]))
+    r['parent-govuks'] = array_to_string(r['parent_organisations'], lambda x: x['id'].rsplit('/', 1)[1])
+
+    print(sep.join([json_path(r, field_path[field]) for field in fields]))
