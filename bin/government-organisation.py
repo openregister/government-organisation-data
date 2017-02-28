02ad5e440cb1bd98ec5c6e672f86ec6a06345f59
Paul Downey <psd@whatfettle.com>
2017-02-28 09:12:43 +0000

    Removed date values of 'closed'

--- bin/government-organisation.py
+++ bin/government-organisation.py
@@ -28,4 +28,7 @@ for row in csv.DictReader(sys.stdin, delimiter=sep):
     row['start-date'] = defval(row, 'start-date')
     row['end-date'] = defval(row, 'end-date')
 
+    if row['end-date'] == 'closed':
+        row['end-date'] = ''
+
     print(sep.join([row[field] for field in fields]))
