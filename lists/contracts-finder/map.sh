3caef82856d54b393898225c8aab82433ebbe4dd
Paul Downey <psd@whatfettle.com>
2017-03-01 18:57:57 +0000

    Moved to buyer-id for contracts-finder

--- lists/contracts-finder/map.sh
+++ lists/contracts-finder/map.sh
@@ -2,5 +2,5 @@
 
 # <label for="buyer_id_220512" class=" selected" >Bedford Borough Council</label>
 
-echo "id	name"
+echo "buyer-id	name"
 grep ' for="buyer_id_' | sed -e 's/^.*buyer_id_//' -e 's/<\/label>.*$//' -e 's/" *class.*>/	/'
