c749d1ee08c93c51f9c49827ea037593a49f179c
Paul Downey <psd@whatfettle.com>
2017-03-01 13:21:15 +0000

    Include all GOV.UK organisations, fixup

--- bin/government-organisation.py
+++ bin/government-organisation.py
@@ -6,18 +6,6 @@ import csv
 fields = ['government-organisation', 'name', 'website', 'start-date', 'end-date']
 sep = '\t'
 
-types = [
-    'Devolved administration',
-    'Executive office',
-    'Ministerial department',
-    'Non-ministerial department',
-    'Advisory non-departmental public body',
-    'Civil Service',
-    'Executive agency',
-    'Executive non-departmental public body'
-]
-
-
 # fixup by website
 website = {}
 for row in csv.DictReader(open(sys.argv[1]), delimiter=sep):
@@ -34,8 +22,8 @@ def default(row, field):
     return ''
 
 def usurp(row, field):
-    if row['website'] in website and row[field]:
-        return website[row['website']].get(field, '')
+    if row['website'] in website and website[row['website']][field]:
+        return website[row['website']][field]
 
     return row[field]
 
@@ -43,13 +31,11 @@ print(sep.join(fields))
 
 for row in csv.DictReader(sys.stdin, delimiter=sep):
 
-    if row['government-organisation-type'] in types:
+    row['name'] = usurp(row, 'name')
+    row['start-date'] = default(row, 'start-date')
+    row['end-date'] = default(row, 'end-date')
 
-        row['name'] = usurp(row, 'name')
-        row['start-date'] = default(row, 'start-date')
-        row['end-date'] = default(row, 'end-date')
+    if row['end-date'] == 'closed':
+        row['end-date'] = ''
 
-        if row['end-date'] == 'closed':
-            row['end-date'] = ''
-
-        print(sep.join([row[field] for field in fields]))
+    print(sep.join([row[field] for field in fields]))
