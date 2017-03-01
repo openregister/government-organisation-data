1431868a8f0b8172862b21c558856ed77dd7aca2
Paul Downey <psd@whatfettle.com>
2017-03-01 18:46:17 +0000

    ONS titles row

--- lists/ons-public-sector-classification/map.py
+++ lists/ons-public-sector-classification/map.py
@@ -6,6 +6,8 @@ from xlrd import open_workbook
 book = open_workbook(sys.argv[1])
 sheet = book.sheet_by_name('Index')
 
+print('name')
+
 for row in range(sheet.nrows):
     s = sheet.cell(row, 0).value
     if s == '' or s == 'Index' or s == 'Institutions':
