034b66efba0e72d2d183d56d7ff172fe9f536de5
Paul Downey <psd@whatfettle.com>
2017-03-02 11:21:19 +0000

    HTML unescape contracts-finder

--- lists/contracts-finder/map.sh
+++ lists/contracts-finder/map.sh
@@ -3,4 +3,11 @@
 # <label for="buyer_id_220512" class=" selected" >Bedford Borough Council</label>
 
 echo "buyer-id	name"
-grep ' for="buyer_id_' | sed -e 's/^.*buyer_id_//' -e 's/<\/label>.*$//' -e 's/" *class.*>/	/'
+grep ' for="buyer_id_' |
+  sed -e 's/^.*buyer_id_//' \
+      -e 's/<\/label>.*$//' \
+      -e 's/" *class.*>/	/' \
+      -e 's/&amp;/\&/g' \
+      -e "s/&#039;/'/g" \
+      -e 's/&eacute;/é/g' \
+      -e 's/&ccedil;/ç/g'
